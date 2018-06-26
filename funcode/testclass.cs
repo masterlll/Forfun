2b0a786c5df1b173de84a760dfc320fb4e062fc0
黃啟琳 <a3697782@hotmail.com>
2018-06-26 23:51:33 +0800

    [Todo] for fun testclass

---
+++ funcode/testclass.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace funcode
+{
+    public class Solution
+    {
+        public int[] TwoSum(int[] nums, int target)
+        {
+
+
+            return null;
+        }
+    }
+}
---
+++ funcodeTests/SolutionTests.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using funcode;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace funcode.Tests
+{
+    [TestClass()]
+    public class SolutionTests
+    {
+
+        private Solution Solution = null;
+
+        public void init()
+        {
+            Solution = new Solution();
+        }
+
+        [TestMethod()]
+        public void TwoSumTest()
+        {
+
+
+            Assert.Fail();
+        }
+    }
+}
